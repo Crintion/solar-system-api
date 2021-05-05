8a16514487f5888ed2d2262349e6ed20e524c58d
Christian <Codedreamer20@gmail.com>
2021-05-04 18:39:46 -0700

    Added delete and put HTTP methods

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ solar_system_bp = Blueprint("solar_system", __name__, url_prefix='/planets')
 
 
 @solar_system_bp.route('', methods=['GET', 'POST'])
-def handle_books():
+def handle_planets():
     if request.method == 'GET':
         #return full list of planets
         solar_system = Planet.query.all()
@@ -20,11 +20,48 @@ def handle_books():
             })
         return jsonify(solar_system_response)
     elif request.method == 'POST':
-        #create a planet to the database
+        #create all planets to the database
         request_body = request.get_json()
         new_planet = Planet(name=request_body['name'],
                         description=request_body['description'])
         db.session.add(new_planet)
         db.session.commit()
         return make_response(f'Planet {new_planet.name} successfully created', 201)
+
+
+@solar_system_bp.route("/<a_planet_id>", methods=['GET', 'DELETE', 'PUT'])
+def handle_planet(a_planet_id):
+    planet = Planet.query.get(a_planet_id)
+    if not planet:
+        return ({
+            "message": f"Planet with id {a_planet_id} was not found",
+            "success": False
+        }, 404)
+    if request.method == 'GET':
+        return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        }, 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(planet)
+        db.session.commit()
+        return ({
+            "message": f"Planet with id {a_planet_id} was deleted",
+            "success": True
+        }, 200) 
+
+    elif request.method == 'PUT':
+        if "name" in request_body:
+            planet.name = request_body["name"]
+        if "description" in request_body:
+            planet.description = request_body["description"]
+
+    
+    
+
+    
+
+
     
